8327c7110cbef85ae98307df21f1de3caac8c84a
kusmayudaedo <kusmayuda.edo@gmail.com>
2023-12-30 19:10:01 +0700

    Fix update user

--- middlewares/multerMiddleware.js
+++ middlewares/multerMiddleware.js
@@ -1,16 +1,27 @@
 import multer from 'multer';
 import path from 'path';
+import DataParser from 'datauri/parser.js';
+import { log } from 'console';
 
-const storage = multer.diskStorage({
-	destination: (req, file, cb) => {
-		cb(null, path.join(process.cwd(), 'public', 'uploads'));
-	},
-	filename: (req, file, cb) => {
-		const filename = file.originalname;
-		cb(null, filename);
-	},
-});
+// const storage = multer.diskStorage({
+// 	destination: (req, file, cb) => {
+// 		cb(null, path.join(process.cwd(), 'public', 'uploads'));
+// 	},
+// 	filename: (req, file, cb) => {
+// 		const filename = file.originalname;
+// 		cb(null, filename);
+// 	},
+// });
+
+const storage = multer.memoryStorage();
 
 const upload = multer({ storage });
+
+const parser = new DataParser();
+
+export const formatImage = (file) => {
+	const fileExtension = path.extname(file.originalname).toString();
+	return parser.format(fileExtension, file.buffer).content;
+};
 
 export default upload;
