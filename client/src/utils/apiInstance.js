bd382e2a7b91bea8209ef388323baf4a1511d5d4
kusmayudaedo <kusmayuda.edo@gmail.com>
2024-01-21 17:40:20 +0700

    Update : update base url on frontend

--- client/src/utils/apiInstance.js
+++ client/src/utils/apiInstance.js
@@ -1,7 +1,7 @@
-import axios from "axios";
+import axios from 'axios';
 
 const api = axios.create({
-  baseURL: "/api/v1",
+	baseURL: 'https://kerja-id.onrender.com/api/v1',
 });
 
 export default api;
